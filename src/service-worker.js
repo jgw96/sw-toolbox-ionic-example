3b31c2b64d23761f8e1c33e5c8f0556e612c01e2
Justin Willis <jgw9617@gmail.com>
2016-12-15 00:11:49 -0600

    slight change

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,6 @@
 /**
  * Check out https://googlechrome.github.io/sw-toolbox/docs/master/index.html for
- * more info on how to use sw-toolbx.
+ * more info on how to use sw-toolbox to custom configure your service worker.
  */
 
 
